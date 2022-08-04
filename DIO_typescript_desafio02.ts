2c767fbfeec95c605b4889a236e022ce676137a6
Rafael <rafael.dimoura@gmail.com>
2022-08-03 22:27:57 -0300

    Desafio Dio Typescript
    Desafio feito para o DIO Typescript

---
+++ DIO_typescript_desafio02.ts
@@ -0,0 +1,23 @@
+let pessoa1 = {
+    nome: "rafael",
+    idade: 34,
+    profissao: "orcamentista"
+};
+
+let pessoa2 = {
+    nome: "patricia",
+    idade: 29,
+    profissao: "psicologa"
+};
+
+let pessoa3 = {
+    nome: "ruane",
+    idade: "28",
+    profissao: "arquiteta"
+};
+
+let pessoa4 = {
+    nome: "tatiane",
+    idade: 40,
+    profissao: "marketing"
+}
